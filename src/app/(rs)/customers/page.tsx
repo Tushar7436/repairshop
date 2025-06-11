ceab301d3d9950aa4dcc22468abb8b9b1c0bdc07
Tushar Agarwal <tushar7436@gmail.com>
2025-06-11 23:56:17 +0530

    Permission and Separation of Concerns

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,6 +1,7 @@
 import CustomerSearch from "@/app/(rs)/customers/CustomerSearch"
 import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults"
-
+import * as Sentry from "@sentry/nextjs"
+import CustomerTable from "@/app/(rs)/customers/form/CustomerTable"
 
 export const metadata = {
     title: "Customers Search",
@@ -14,13 +15,20 @@ export default async function Customers({
     const { searchText } = await searchParams
 
     if(!searchText) return <CustomerSearch />
-
+    
+    const span = Sentry.startInactiveSpan({
+        name: 'getCustomerSearchResults-2'
+    })
     //query database
     const results = await getCustomerSearchResults(searchText)
+    span.end()
 
     return (
         <>
         <CustomerSearch />
-        <p> {JSON.stringify(results)}</p></>
+        {results.length ? <CustomerTable data={results} />: (
+            <p className="mt-4">No results found</p>
+        )}
+        </>
     )
 }
